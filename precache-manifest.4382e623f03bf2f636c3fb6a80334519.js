c420f75e1e2163bfdb745fd78304feb8f34414f0
Philipp Rivadeneira <philmnap@gmail.com>
2019-10-17 21:35:29 -0500

    Updates

---
+++ precache-manifest.4382e623f03bf2f636c3fb6a80334519.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbcbc9d00bde7f8077839d32c4bb4a8",
+    "url": "/Philmnap/clicky-game-/index.html"
+  },
+  {
+    "revision": "4b63d9cfe17231c2495b",
+    "url": "/Philmnap/clicky-game-/static/css/main.2b584879.chunk.css"
+  },
+  {
+    "revision": "9501f22ec6282b72990a",
+    "url": "/Philmnap/clicky-game-/static/js/2.99673906.chunk.js"
+  },
+  {
+    "revision": "4b63d9cfe17231c2495b",
+    "url": "/Philmnap/clicky-game-/static/js/main.c3010570.chunk.js"
+  },
+  {
+    "revision": "32cee59a6abce1f41589",
+    "url": "/Philmnap/clicky-game-/static/js/runtime-main.a96ec924.js"
+  },
+  {
+    "revision": "c865dfd419fe9a181cc552d147e65857",
+    "url": "/Philmnap/clicky-game-/static/media/Locain.c865dfd4.jpg"
+  },
+  {
+    "revision": "ce15b0d8259de94cbb7278a8b6b65909",
+    "url": "/Philmnap/clicky-game-/static/media/backround.ce15b0d8.jpg"
+  },
+  {
+    "revision": "ea15f4202bf4429e48781359eac33f85",
+    "url": "/Philmnap/clicky-game-/static/media/bellinger.ea15f420.jpg"
+  },
+  {
+    "revision": "b4f456cae5a88f7457af6ed2fdd284c5",
+    "url": "/Philmnap/clicky-game-/static/media/bryce.b4f456ca.jpg"
+  },
+  {
+    "revision": "accfe8f884c1016b0614a2e6d69f2795",
+    "url": "/Philmnap/clicky-game-/static/media/degrom.accfe8f8.jpg"
+  },
+  {
+    "revision": "e9b8bb7110b7588727b80fbd4d2ffbe7",
+    "url": "/Philmnap/clicky-game-/static/media/frustrated.e9b8bb71.jpg"
+  },
+  {
+    "revision": "ee4708da6d7add36fdd6172d490b5f8d",
+    "url": "/Philmnap/clicky-game-/static/media/gerrit.ee4708da.jpg"
+  },
+  {
+    "revision": "022c51f6cf11f9c1edca42ce7aaa4e7b",
+    "url": "/Philmnap/clicky-game-/static/media/happy_player.022c51f6.jpg"
+  },
+  {
+    "revision": "9d0ad93a22b2d57ca89644085fb456f0",
+    "url": "/Philmnap/clicky-game-/static/media/judge.9d0ad93a.jpg"
+  },
+  {
+    "revision": "00f4401449ef39dc8f7b8986a4c3734a",
+    "url": "/Philmnap/clicky-game-/static/media/lindor.00f44014.jpg"
+  },
+  {
+    "revision": "eea717157dead098deb74a63fe8fa168",
+    "url": "/Philmnap/clicky-game-/static/media/mookie-betts.eea71715.jpg"
+  },
+  {
+    "revision": "1ec0be17e36669e8d2baa5dc0e6d0af8",
+    "url": "/Philmnap/clicky-game-/static/media/yelich.1ec0be17.jpg"
+  }
+]);
